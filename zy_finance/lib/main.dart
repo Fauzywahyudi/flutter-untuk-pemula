0ee43da95b9cd48199a770f3830c2a1280d4838c
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-12-22 09:02:02 +0700

    remove theme

--- zy_finance/lib/main.dart
+++ zy_finance/lib/main.dart
@@ -1,49 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:zy_finance/src/config/router.gr.dart';
-import 'package:zy_finance/src/provider/dark_theme.dart';
-import 'package:zy_finance/src/theme/theme.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  DarkThemeProvider themeChangeProvider = new DarkThemeProvider();
-
-  @override
-  void initState() {
-    super.initState();
-    getCurrentAppTheme();
-  }
-
-  void getCurrentAppTheme() async {
-    themeChangeProvider.darkTheme =
-        await themeChangeProvider.darkThemePreference.getTheme();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) {
-        return themeChangeProvider;
-      },
-      child: Consumer<DarkThemeProvider>(
-        builder: (BuildContext context, value, Widget child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            initialRoute: Router.splashScreen,
-            onGenerateRoute: Router.onGenerateRoute,
-            navigatorKey: Router.navigatorKey,
-            theme: MyTheme.themeData(themeChangeProvider.darkTheme, context),
-          );
-        },
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Router.splashScreen,
+      onGenerateRoute: Router.onGenerateRoute,
+      navigatorKey: Router.navigatorKey,
     );
   }
 }
--- zy_finance/lib/src/pages/home/home_page.dart
+++ zy_finance/lib/src/pages/home/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:zy_finance/src/provider/dark_theme.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,28 +8,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final stateTheme = Provider.of<DarkThemeProvider>(context);
-
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(title: Text('Home')),
-            ListTile(title: Text('Profil')),
-            SwitchListTile(
-              title: Text('Dark Mode'),
-              value: stateTheme.darkTheme,
-              onChanged: (value) => stateTheme.darkTheme = value,
-            )
-          ],
-        ),
-      ),
       body: Container(),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: null,
-      //   child: Icon(Icons.add),
-      // ),
     );
   }
 }
